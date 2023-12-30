d7b274aa81a2b3ffa171c665205a050cd7413cb0
Temidayo <nifetemiboy@gmail.com>
2023-12-30 21:19:03 +0100

    motion and home page creation

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import state from '../store'
+import {
+    motion,
+    AnimatePresence
+} from 'framer-motion'
+import { useSnapshot } from 'valtio'
+import {
+    headContainerAnimation,
+    headContentAnimation,
+    headTextAnimation,
+    slideAnimation
+} from '../config/motion'
+
+const Home = () => {
+    const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+        {snap.intro &&  (
+            <motion.section className='home' {...slideAnimation('left')}>
+                <motion.header {...slideAnimation('down')}>
+                    <img src="./threejs.png" alt="logo"  className='w-8 h-8 object-contain'/>
+                </motion.header>
+
+                <motion.div className='home-content' {...headContentAnimation}>
+                    <motion.div {...headTextAnimation}>  
+                        <h1 className="head-text">
+                            LET'S <br className='xl:block hidden'/> DO IT
+                        </h1> 
+                    </motion.div>
+
+                    <motion.div>
+                        <p className='max-w-md'>
+                            Create your unique and exclusive shirt with our brand new 3D cstomization tool.
+                            <strong>Unleash your imagination</strong>{''} and define your own style
+                        </p>
+                    </motion.div>
+                </motion.div>
+            </motion.section>
+        )}
+    </AnimatePresence>
+  )
+}
+
+export default Home
