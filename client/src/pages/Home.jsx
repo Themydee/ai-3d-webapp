e8b965a4d8d1e6633ebc2c2107d64edf3a173ce9
Temidayo <nifetemiboy@gmail.com>
2024-01-01 21:29:14 +0100

    button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import state from '../store'
+import { CustomButton } from '../components'
 import {
     motion,
     AnimatePresence
@@ -29,11 +30,20 @@ const Home = () => {
                         </h1> 
                     </motion.div>
 
-                    <motion.div>
-                        <p className='max-w-md'>
+                    <motion.div
+                    {...headContentAnimation}
+                    className='flex flex-col gap-5'>
+                        <p className='max-w-md font-normal text-gray-600 text-base'>
                             Create your unique and exclusive shirt with our brand new 3D cstomization tool.
                             <strong>Unleash your imagination</strong>{''} and define your own style
                         </p>
+
+                        <CustomButton 
+                            type="filled"
+                            title="Customize It"
+                            handleClick={() => state.intro = false}
+                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+                        />
                     </motion.div>
                 </motion.div>
             </motion.section>
